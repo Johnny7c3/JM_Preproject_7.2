3b713740c2c5ae72afec2e883cd07fca9cd3f8b2
Johnny7c3 <johnny7c3@gmail.com>
2020-03-21 00:11:55 +0300

    Correction_1

--- src/main/java/ru/javamentor/preproject/controller/HelloController.java
+++ src/main/java/ru/javamentor/preproject/controller/HelloController.java
@@ -1,12 +1,9 @@
 package ru.javamentor.preproject.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import ru.javamentor.preproject.model.Car;
-import ru.javamentor.preproject.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
